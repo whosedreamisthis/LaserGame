932c850ea3f7d8801ea066cc1f936b5abd1dc8b5
Dana Sharon <whosedreamisthis@gmail.com>
2023-05-20 14:23:04 -0700

    taking damage

---
+++ Assets/Scripts/Health.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+    [SerializeField] int health = 50;
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        DamageDealer damageDealer = other.GetComponent<DamageDealer>();
+        if (damageDealer != null && tag != other.tag)
+        {
+            TakeDamage(damageDealer.GetDamage());
+            //tell damage dealer that it hit something
+            damageDealer.Hit();
+        }
+    }
+
+    void TakeDamage(int damageAmount)
+    {
+        health -= damageAmount;
+        if (health <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+}
