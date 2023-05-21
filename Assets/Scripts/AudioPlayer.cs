c54fb02c611262824a1668cc6382a57f8e05f909
Dana Sharon <whosedreamisthis@gmail.com>
2023-05-21 10:43:48 -0700

    score on game over screen

--- Assets/Scripts/AudioPlayer.cs
+++ Assets/Scripts/AudioPlayer.cs
@@ -11,7 +11,26 @@ public class AudioPlayer : MonoBehaviour
     [Header("Damage")]
     [SerializeField] AudioClip damageClip;
     [SerializeField][Range(0f, 1f)] float damageVolume = 0.5f;
+    static AudioPlayer instance;
 
+
+    void Awake()
+    {
+        ManageSingleton();
+    }
+    void ManageSingleton()
+    {
+        if (instance != null)
+        {
+            gameObject.SetActive(false);
+            Destroy(gameObject);
+        }
+        else
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+    }
     public void PlayShootingClip()
     {
         PlayClip(shootingClip, shootingVolume);
